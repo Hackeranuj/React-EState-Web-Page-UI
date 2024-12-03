42797fcc225e7449724045ba8d32a46c2b3c4cac
Anuj Gahra <anujgahra@Anujs-MacBook-Air.local>
2024-12-03 10:09:35 +0530

    Real-EState

---
+++ src/utils/common.js
@@ -0,0 +1,19 @@
+export const sliderSettings = {
+    slidesPerView: 1, // Corrected property name
+    spaceBetween: 50,
+    breakpoints: {
+      480: {
+        slidesPerView: 1, // Corrected property name
+      },
+      600: {
+        slidesPerView: 2, // Corrected property name
+      },
+      750: {
+        slidesPerView: 3, // Corrected property name
+      },
+      1100: {
+        slidesPerView: 4, // Corrected property name
+      },
+    },
+  };
+  
